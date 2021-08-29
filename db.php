bce9ba4c5a8e4d71ce27ccd328394f6801ebe2c4
Mahendra067 <89721075+Mahendra067@users.noreply.github.com>
2021-08-29 22:25:43 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","","food-order");
+    $con = mysqli_connect("remotemysql.com","DI2ESSb1O4","","DI2ESSb1O4");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
