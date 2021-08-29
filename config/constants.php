42be6e5e654e2fce2e8ecd79a4f7163efcaae292
Mahendra067 <89721075+Mahendra067@users.noreply.github.com>
2021-08-29 22:20:13 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -11,11 +11,11 @@
     // define('DB_NAME', 'food-order');
 
     //Remote Database Connection
-    define('SITEURL', 'http://remotemysql.com/1LxGQXZvd0/');
+    define('SITEURL', 'http://remotemysql.com/DI2ESSb1O4/');
     define('LOCALHOST', 'remotemysql.com');
-    define('DB_USERNAME', '1LxGQXZvd0');
+    define('DB_USERNAME', 'DI2ESSb1O4');
     define('DB_PASSWORD', '');
-    define('DB_NAME', '1LxGQXZvd0');
+    define('DB_NAME', 'DI2ESSb1O4');
 
     //execute query and save data in database
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());//database connection
