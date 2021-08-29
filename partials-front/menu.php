cf387324a7fc06c3111b658ec1b5587f65eb2aef
Mahendra067 <89721075+Mahendra067@users.noreply.github.com>
2021-08-29 21:56:15 +0530

    Update menu.php

--- partials-front/menu.php
+++ partials-front/menu.php
@@ -1,11 +1,19 @@
 <?php
 include("auth_session.php");
+    //devlopement connection
     //create constants to store non repeating value
-    define('SITEURL', 'http://localhost/food-order/');
-    define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
+    // define('SITEURL', 'http://localhost/food-order/');
+    // define('LOCALHOST', 'localhost');
+    // define('DB_USERNAME', 'root');
+    // define('DB_PASSWORD', '');
+    // define('DB_NAME', 'food-order');
+
+    //Remote Database Connection
+    define('SITEURL', 'http://remotemysql.com/food-order/');
+    define('LOCALHOST', 'remotemysql.com');
+    define('DB_USERNAME', '1LxGQXZvd0');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('DB_NAME', '1LxGQXZvd0');
 
     //execute query and save data in database
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());//database connection
